9857ec032f0a8b55952f5b1d59861a57a113af82
0verc10ck <31722109+0verc10ck@users.noreply.github.com>
2019-11-19 13:01:11 +0900

    modified viewpager
    remove fragment and added picture array

---
+++ student_app/app/src/main/java/com/student/app/ViewpagerAdapter.kt
@@ -0,0 +1,49 @@
+package com.student.app
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.firebase.storage.FirebaseStorage
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+class ViewPagerAdapter(private val context : Context) : PagerAdapter() {
+
+    private var layoutInflater : LayoutInflater? = null
+    val storageReference = FirebaseStorage.getInstance().reference
+    var Image = listOf(R.drawable.A)
+
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view ===  `object`
+    }
+
+    override fun getCount(): Int {
+        return Image.size
+    }
+
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val v = layoutInflater!!.inflate(R.layout.viewpager, null)
+        val image = v.findViewById<View>(R.id.vpImgview) as ImageView
+
+        image.setImageResource(Image[position])
+        val vp = container as ViewPager
+        vp.addView(v , 0)
+
+
+        return v
+
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        val vp = container as ViewPager
+        val v = `object` as View
+        vp.removeView(v)
+    }
+}
--- student_app/app/src/main/java/com/student/app/studentActivity.kt
+++ student_app/app/src/main/java/com/student/app/studentActivity.kt
@@ -1,21 +1,20 @@
 package com.student.app
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.charge_page.*
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.student_page.*
 
-
 class studentActivity : AppCompatActivity() {
-    var viewList = ArrayList<View>()
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    internal lateinit var viewpager : ViewPager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.student_page)
 
@@ -24,8 +23,9 @@ class studentActivity : AppCompatActivity() {
             startActivity(nextIntent)
         }
 
-        val database : FirebaseDatabase = FirebaseDatabase.getInstance()
-        val charge : DatabaseReference = database.getReference("student").child(student.ID).child("money")
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+        val charge: DatabaseReference =
+            database.getReference("student").child(student.ID).child("money")
 
         charge.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -40,38 +40,15 @@ class studentActivity : AppCompatActivity() {
         })
 
 
+        /*
+        val storage = FirebaseStorage.getInstance()
+        var storageRef = storage.reference
+        val aRef = storageRef.child("A")
+         */
 
-        viewList.add(layoutInflater.inflate(R.layout.page_a, null))
-        viewList.add(layoutInflater.inflate(R.layout.page_b, null))
-        viewList.add(layoutInflater.inflate(R.layout.page_c, null))
-        viewList.add(layoutInflater.inflate(R.layout.page_d, null))
-
-        pager.adapter = pagerAdapter()
-
-
-    }
-
-
-    inner class pagerAdapter : PagerAdapter()
-    {
-        override fun getCount(): Int{
-            return viewList.size
+        viewpager = findViewById(R.id.pager) as ViewPager
+        var adapter = ViewPagerAdapter(this)
+        viewpager.adapter = adapter
         }
-
-        override fun isViewFromObject(view: View, `object`: Any): Boolean {
-            return view == `object`
-        }
-
-        override fun instantiateItem(container: ViewGroup, position: Int): Any {
-            var temp = viewList[position]
-            pager.addView(temp)
-            return viewList[position]
-        }
-
-        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-            pager.removeView(`object` as View)
-        }
-    }
 }
-
 
