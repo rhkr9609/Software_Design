05a128d1291e3e45af0a80531bea4b6e0c666c0f
rhkr9609 <45326283+rhkr9609@users.noreply.github.com>
2019-12-16 12:54:19 +0900

    StudentChargeMoney
    StudentChargeMoney

--- student/app/src/main/java/com/student/app/studentActivity.kt
+++ student/app/src/main/java/com/student/app/studentActivity.kt
@@ -1,11 +1,42 @@
 package com.student.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.student_page.*
 
 class studentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.student_page)
+
+        val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+        btn_balance.setOnClickListener {
+            val nextIntent = Intent(this, chargeActivity::class.java)
+            startActivity(nextIntent)
+        }
+        //databases 호출 부분
+        val charge : DatabaseReference = database.getReference("student").child(student.ID).child("money")
+        val order : DatabaseReference = database.getReference("student").child(student.ID).child("order")
+        charge.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot?.value
+                textView3.text = "$value 원"
+                student.money = "$value".toInt()
+            }
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+        order.addValueEventListener(object  : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val order_state = dataSnapshot?.value
+                student.order = "$order_state"
+            }
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 }
