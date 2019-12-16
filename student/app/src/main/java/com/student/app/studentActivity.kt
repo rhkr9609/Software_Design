8fa342ee954ad655368a1b69843dbbce5f545ce8
rhkr9609 <45326283+rhkr9609@users.noreply.github.com>
2019-12-16 11:05:15 +0900

    loginAndsignUp
    loginAndsignUp

---
+++ student/app/src/main/java/com/student/app/studentActivity.kt
@@ -0,0 +1,11 @@
+package com.student.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class studentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.student_page)
+    }
+}
